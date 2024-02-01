593c044ea0cf50b3f3b47bbe1ac4ecec7edf332f
unknown <dongalakrishnaiah371@gmail.com>
2024-02-01 23:22:29 +0530

    ReactProject

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import EmojiSearching from "./emojiSearch/emoji_Search";
+
+function App() {
+  return (
+    <>
+      <EmojiSearching />
+    </>
+  );
+}
+
+export default App;
---
+++ src/emojiSearch/emoji_Search.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import EmojiPicker, { Emoji } from "emoji-picker-react";
+
+
+const EmojiSearching = () => {
+  const [emoji, setEmoji] = useState("");
+  const handleEmojiClick = (emojiData) => {
+    setEmoji(emojiData.unified);
+  };
+  return (
+    <center>
+    <div>
+      <h1>Emoji Searching </h1>
+      <div>{emoji ? <Emoji unified={emoji} size={50}/> : null}</div>
+      <EmojiPicker onEmojiClick={handleEmojiClick} />
+    </div>
+    </center>
+   
+  );
+};
+
+export default EmojiSearching;
